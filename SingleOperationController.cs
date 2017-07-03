1dc3b7c1f1444b7d609030920842ef323a98e903
AndreyStelmakh <andrey.stelmakh@live.ru>
2017-07-03 22:59:24 +0300

    First commit

---
+++ SingleOperationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NoShake
+{
+    public class SingleOperationController
+    {
+        public void StartOperationWeak()
+        {
+            lock(_sO)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public void StartOperationStrong()
+        {
+            lock (_sO)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private object _sO = new object();
+    }
+}
